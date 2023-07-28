b5e7fe7be7514ccda1202540d4b477d83280bd8b
Sharafadeen Hammed <sharafadeenhammed@yahoo.com>
2023-07-28 09:17:53 +0100

    testing out messaging functionalty

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import morgan from "morgan";
 // import colors from "colors";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import axios from "axios";
 import connectDb from "./config/db.js";
 import errorHandeler from "./utils/errorHandler.js";
 import auth from "./routes/auth.js";
@@ -31,6 +32,32 @@ app.use(express.static("public"));
 
 app.get("/", (req, res, next) => {
   res.redirect("https://documenter.getpostman.com/view/20324776/2s946iaqNP");
+});
+
+app.get("/message", async (req, res, next) => {
+  const reqInstance = axios.create({
+    headers: {
+      Authorization: "Bearer ac56aa4016d2425fa80a9115da2dffc4",
+      "Content-Type": "application/json",
+    },
+  });
+  try {
+    const response = await reqInstance.post(
+      "https://us.sms.api.sinch.com/xms/v1/cd2a975c633c4eba84fa3e4ed3036590/batches",
+
+      {
+        from: "447520662453",
+        to: ["2348073191813"],
+        body: "irrigation pump turned on",
+      }
+    );
+
+    if (response.status < 200 && response.status > 300) throw res.data;
+    console.log(response);
+    res.status(200).json({ status: "okay" });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.use("/api/v1/auth", auth);
