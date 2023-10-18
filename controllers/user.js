a0b1590cbfb254e4f885c859f07eaa48e21c75c4
Sharafadeen Hammed <sharafadeenhammed@yahoo.com>
2023-10-18 17:12:50 +0100

    handeled removal of unused user images on the server

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 import path from "path";
+import fs from "fs";
 import { fileURLToPath } from "url";
 
 import ErrorResponse from "../utils/errorResponse.js";
@@ -34,6 +35,18 @@ export const photoUpload = asyncHandeler(async (req, res, next) => {
     return next(new ErrorResponse("image size must not be more than 1mb", 400));
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
+
+  // remove initial uploaded files on the server
+  imageExtension.forEach((item) => {
+    fs.unlink(
+      path.join(__dirname, "..", "public", req.user.id + item),
+      (err) => {
+        if (err) console.log(err);
+        console.log(`${req.user.id}${item} deleted`);
+      }
+    );
+  });
+
   req.files.photo.mv(
     path.join(__dirname, "..", "public", req.user.id + file.ext)
   );
