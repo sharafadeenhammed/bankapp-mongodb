1caada9d62b2d8804f62a09bfeab08503ef2b318
Sharafadeen Hammed <sharafadeenhammed@yahoo.com>
2023-10-23 00:11:55 +0100

    testing changes

--- controllers/user.js
+++ controllers/user.js
@@ -47,12 +47,13 @@ export const photoUpload = asyncHandeler(async (req, res, next) => {
     );
   });
 
-  req.files.photo.mv(
+  await req.files.photo.mv(
     path.join(__dirname, "..", "public", req.user.id + file.ext)
   );
   const user = await UserModel.findByIdAndUpdate(req.user.id, {
     photo_url: `https://${process.env.ASSET_URL}/${req.user.id}${file.ext}`,
   });
+  // const updatedUSer = await UserModel.findById(req.user.id);
   res.status(200).json({
     success: true,
     message: "photo uploaded successful",
